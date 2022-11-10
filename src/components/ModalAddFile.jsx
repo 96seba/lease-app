463d0ab2026be17dc40c7ba110d0698ab0623562
BenjaJara <benjamin.jara1701@alumnos.ubiobio.cl>
2022-11-10 17:40:06 -0300

    add modalAddFile dashboard

---
+++ src/components/ModalAddFile.jsx
@@ -0,0 +1,95 @@
+import { Fragment, useRef, useState } from 'react'
+import { Dialog, Transition } from '@headlessui/react'
+import { BsSaveFill } from "react-icons/bs";
+
+
+
+
+
+export default function ModalGuardar({ open, setOpen }) {
+
+    const[file,setFile]=useState("");
+
+    const cancelButtonRef = useRef(null);
+
+
+    const checkExtension=(str)=>{
+        let arr = str.split('.');
+        let ext = arr[arr.length-1];
+        console.log(ext)
+
+        if(ext === 'xls' || ext === 'xlsx' || ext === 'csv'){
+            setOpen(false)
+            // llamar api
+        }
+        else{
+            alert("El archivo debe ser de tipo excel (.xls, .xlsx o .csv)");
+        }
+
+    }
+
+    return (
+        <Transition.Root show={open} as={Fragment}>
+            <Dialog as="div" className="relative z-10" initialFocus={cancelButtonRef} onClose={setOpen}>
+                <Transition.Child
+                    as={Fragment}
+                    enter="ease-out duration-1000"
+                    enterFrom="opacity-0"
+                    enterTo="opacity-100"
+                    leave="ease-in duration-1000"
+                    leaveFrom="opacity-100"
+                    leaveTo="opacity-0"
+                >
+                    <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+                </Transition.Child>
+
+                <div className="fixed inset-0 z-10 overflow-y-auto">
+                    <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+                        <Transition.Child
+                            as={Fragment}
+                            enter="ease-out duration-300"
+                            enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                            enterTo="opacity-100 translate-y-0 sm:scale-100"
+                            leave="ease-in duration-200"
+                            leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+                            leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                        >
+                            <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                                <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4 flex justify-center">
+
+                                    <div className="sm:flex sm:items-start">
+
+                                        <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+
+                                            <Dialog.Title as="h3" className="text-lg font-medium flex flex-col justify-center items-center leading-6 text-gray-900">
+                                            <input value={file} onChange={e=>{setFile(e.target.value)
+                                            console.log(e.target.value.split('.'))
+                                                }} id="fileSelect" type="file" accept=".csv, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel" />
+                                                
+                                            </Dialog.Title>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse flex justify-center items-center sm:px-6">
+                                    <button
+                                        type="button"
+                                        className="inline-flex w-[40%] justify-center rounded-md border border-transparent bg-teal-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-teal-500 focus:ring-offset-2 sm:ml-3 sm:text-sm"
+                                        onClick={() => checkExtension(file)} >
+                                        Guardar
+                                    </button>
+                                    <button
+                                        type="button"
+                                        className="inline-flex w-[40%] justify-center rounded-md border border-transparent bg-gray-100 px-4 py-2 text-base font-medium text-teal-600 shadow-sm hover:bg-teal-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-teal-500 focus:ring-offset-2 sm:ml-3 sm:text-sm"
+                                        onClick={() => setOpen(false)}
+                                    >
+                                        Cancelar
+                                    </button>
+                                </div>
+                            </Dialog.Panel>
+                        </Transition.Child>
+                    </div>
+                </div>
+            </Dialog>
+        </Transition.Root>
+    )
+}
--- src/screen/Dashboard.jsx
+++ src/screen/Dashboard.jsx
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import TableCheck from '../components/TableCheck'
 import TableDebtors from '../components/TableDebtors'
 import TableToResolveNVisits from '../components/TableToResolveNVisits';
+import ModalAddFile from '../components/ModalAddFile';
 
 
 
@@ -22,10 +23,18 @@ export default function Dashboard() {
         { id: 81, descripcion: "Revisar la luz", estado: "No revisado" },
     ])
 
+    const [open, setOpen] = useState(false)
+
+    const openModal = (data) => {
+        setOpen(true)
+        // console.log("AAAAAAAAAAAAAA")
+        console.log(data)
+    }
+
     return (
         <div className="flex flex-col h-[91.5vh] bg-gray-50 w-screen items-start justify-start">
             <div className=" flex mt-3 bg-gray-50 h-[5vh] w-screen justify-start px-6">
-                <button className="flex text-white bg-[#374151] hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Subir Excel</button>
+                <button onClick={() => setOpen(true)}  setOpen={setOpen} openModal={openModal} className="flex text-white bg-[#374151] hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Subir Excel</button>
             </div>
             <div className="flex h-[86.5vh] mt-4 bg-gray-50 w-screen items-center justify-center">
                 <div className='flex w-3/4  h-[91.5vh] items-center justify-center flex-col'>
@@ -45,6 +54,7 @@ export default function Dashboard() {
                     <TableCheck dataCheck={dataCheck} setDataCheck={setDataCheck} />
                 </div>
             </div>
+            <ModalAddFile open={open} setOpen={setOpen}/>
         </div>
     )
 } 
